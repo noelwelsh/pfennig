cb97ad1859e10a35e27c60e616518b075effa11f
Noel Welsh <noel@untyped.com>
2016-02-14 22:14:45 +0000

    Cleanup implementation
    Implement probability monad in terms of the free monad. The primitive
    generators are parameterised by a typeclass sampleable. `Random` is
    provided as the default instance but the user can substitute in other
    instances.
    
    Next step is to add conditioning, and then rewrite existing inference
    algorithms in terms of this new representation.

--- src/main/scala/pfennig/Free.scala
+++ src/main/scala/pfennig/Free.scala
@@ -3,15 +3,35 @@ package free
 
 import cats.Comonad
 import cats.free.Free
+import cats.arrow.NaturalTransformation
+import cats.std.function._
+import scala.language.higherKinds
 
-object Distribution {
-  type Distribution[A] = Free[Random,A]
+object distribution {
+  sealed trait Generator[A] {
+    def sample(implicit randomness: Randomness): A
+  }
+  final case class Primitive[F[_],A](distribution: F[A], sampleable: Sampleable[F]) extends Generator[A] {
+    def sample(implicit randomness: Randomness): A =
+      sampleable.sample(distribution)(randomness)
+  }
+
+  def distributionCompiler(implicit randomness: Randomness): NaturalTransformation[Generator, Function0] =
+  new NaturalTransformation[Generator, Function0] {
+    def apply[A](fa: Generator[A]): () => A =
+      () => fa.sample(randomness)
+  }
+
+  type Distribution[A] = Free[Generator,A]
 
   implicit class DistributionOps[A](distribution: Distribution[A]) {
+    def prepare(implicit randomness: Randomness): () => A =
+      distribution.foldMap(distributionCompiler(randomness))
+
     def sample: A =
       sample(scala.util.Random)
 
     def sample(implicit randomness: Randomness): A =
-      distribution.run
+      distribution.prepare.apply()
   }
 }
