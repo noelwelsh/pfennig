e0b02743bd2c3d94750259febc6e338f6649e9af
Noel Welsh <noel@untyped.com>
2016-02-14 22:26:23 +0000

    Add some comments and other general cleanup

--- src/main/scala/pfennig/Free.scala
+++ src/main/scala/pfennig/Free.scala
@@ -8,6 +8,14 @@ import cats.std.function._
 import scala.language.higherKinds
 
 object distribution {
+  // We define this little algebraic data type as we want to hide the type F in
+  // Primitive from external users (i.e. we want it to be an existential) as the
+  // only operation we care about (sampling) doesn't need this type to be known,
+  // and having it known makes life more difficult for us.
+  //
+  // We can't do this directly by declaring an existential (or at least I don't
+  // know how to do it) so we have this arrangement to allow us to use
+  // `Generator` externally that hides the type `F`.
   sealed trait Generator[A] {
     def sample(implicit randomness: Randomness): A
   }
@@ -16,7 +24,8 @@ object distribution {
       sampleable.sample(distribution)(randomness)
   }
 
-  def distributionCompiler(implicit randomness: Randomness): NaturalTransformation[Generator, Function0] =
+  /** Compile a Distribution to a function that, when applied, produces a sample. */
+  def compile(implicit randomness: Randomness): NaturalTransformation[Generator, Function0] =
   new NaturalTransformation[Generator, Function0] {
     def apply[A](fa: Generator[A]): () => A =
       () => fa.sample(randomness)
@@ -26,7 +35,7 @@ object distribution {
 
   implicit class DistributionOps[A](distribution: Distribution[A]) {
     def prepare(implicit randomness: Randomness): () => A =
-      distribution.foldMap(distributionCompiler(randomness))
+      distribution.foldMap(compile(randomness))
 
     def sample: A =
       sample(scala.util.Random)
